0b2b3ac536aa6b89075b097b498f6d61f5286dc7
Jeff Berlin <jeff@jbrally.com>
2018-06-18 22:50:07 -0400

    Adding files

---
+++ contacts/src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class ContactList extends React.Component {
+  render() {
+    const people = [
+      { name: 'Tyler' },
+      { name: 'Karen' },
+      { name: 'Richard' }
+    ]
+
+    return <ol>
+      {people.map((person) => (
+        <li key={person.name}>{person.name}</li>
+      ))}
+    </ol>
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <ContactList />
+        <ContactList />
+        <ContactList />
+      </div>
+    );
+  }
+}
+
+export default App;
